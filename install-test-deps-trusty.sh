a6f11028645239158734162ffa388eb5b1ef85ea
Michael Shuler <michael@pbandjelly.org>
2015-03-26 15:30:29 -0500

    Clone CCM master and pip install checkout

--- install-test-deps-trusty.sh
+++ install-test-deps-trusty.sh
@@ -1,4 +1,6 @@
 #!/bin/sh -xe
 sudo apt-get update -q2
 sudo apt-get -y install ant ant-optional libev-dev libyaml-dev python-dev python-setuptools wamerican
-sudo pip install blist cassandra-driver ccm cql decorator futures nose-test-select pycassa
+sudo pip install blist cassandra-driver cql decorator futures nose-test-select pycassa
+git clone https://github.com/pcmanus/ccm.git
+sudo pip install -e ccm
