4bf68536cc642beff5bffdea2c9003440e92f48b
Alan Boudreault <alan@alanb.ca>
2015-05-07 14:44:12 -0400

    experimental add-gce-data-disk-v2.sh

---
+++ add-gce-data-disk-v2.sh
@@ -0,0 +1,13 @@
+#!/bin/sh -xe
+PARTITION="sda2"
+MOUNTPOINT="/mnt"
+if grep $PARTITION /proc/partitions; then
+    sudo mkfs.ext4 /dev/$PARTITION
+    [ -d $MOUNTPOINT ] || sudo mkdir $MOUNTPOINT
+    sudo mount /dev/$PARTITION $MOUNTPOINT
+    sudo mkdir $MOUNTPOINT/tmp
+    echo "Done Setting Up Data Disk."
+else
+    echo "Data Partition Not Found!"
+    exit 1
+fi
