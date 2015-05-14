cdc659c9d558f5abd77c419cb6425f5586080602
Michael Shuler <michael@pbandjelly.org>
2015-05-14 15:18:46 -0500

    Output java -version, since I removed that from jenkins jobs

--- install-test-deps-trusty.sh
+++ install-test-deps-trusty.sh
@@ -1,6 +1,9 @@
 #!/bin/sh -xe
 # fix default umask of 0002 for hadoop data dir errors
 sudo sh -c "echo umask 0022 >> /etc/profile"
+# which jdk do we have?
+java -version
+
 # update repo metadata and install some basic packages we need
 sudo apt-get update -q2
 sudo apt-get -y install ant ant-optional libev-dev libyaml-dev python-dev python-setuptools wamerican
