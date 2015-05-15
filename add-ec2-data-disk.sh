70ab5b55c4ac24ddc73e3c0b97ae33eab0285e36
Michael Shuler <michael@pbandjelly.org>
2015-05-15 17:31:28 -0500

    Add EC2 test server setup scripts

---
+++ add-ec2-data-disk.sh
@@ -0,0 +1,14 @@
+#!/bin/sh -xe
+PARTITION="xvdb"
+MOUNTPOINT="/mnt"
+if grep $PARTITION /proc/partitions; then
+    sudo mkfs.ext4 /dev/$PARTITION
+    [ -d $MOUNTPOINT ] || sudo mkdir $MOUNTPOINT
+    sudo mount /dev/$PARTITION $MOUNTPOINT
+    sudo mkdir -m 1777 $MOUNTPOINT/tmp
+    sudo sh -c "echo \"export TMPDIR=$MOUNTPOINT/tmp\" >> /etc/profile"
+    echo "Done Setting Up Data Disk."
+else
+    echo "Data Partition Not Found!"
+    exit 1
+fi
