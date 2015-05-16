cf901496aacac7b0edf537bee42f27ac0f00ae2e
Michael Shuler <michael@pbandjelly.org>
2015-05-15 20:57:47 -0500

    Remove apt-get update, since we do that earlier

--- install-test-deps-jessie.sh
+++ install-test-deps-jessie.sh
@@ -4,8 +4,7 @@ sudo sh -c "echo \"umask 0022\" >> /etc/profile"
 # which jdk do we have?
 java -version
 
-# update repo metadata and install some basic packages we need
-sudo apt-get update -q2
+# install some basic packages we need
 sudo apt-get -y install ant ant-optional libev-dev libyaml-dev python-dev python-setuptools wamerican
 # install some python modules that we need
 sudo pip install blist cassandra-driver cql decorator futures nose-test-select pycassa
