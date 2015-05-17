183f0a621071f4aa23ffc5340fe78f4768e5cfcf
Michael Shuler <michael@pbandjelly.org>
2015-05-17 16:30:56 -0500

    Add git rsync and screen to install-test-deps

--- install-test-deps-trusty.sh
+++ install-test-deps-trusty.sh
@@ -1,12 +1,11 @@
 #!/bin/sh -xe
 # fix default umask of 0002 for hadoop data dir errors
-sudo sh -c "echo umask 0022 >> /etc/profile"
+sudo sh -c "echo \"umask 0022\" >> /etc/profile"
 # which jdk do we have?
 java -version
 
-# update repo metadata and install some basic packages we need
-sudo apt-get update -q2
-sudo apt-get -y install ant ant-optional libev-dev libyaml-dev python-dev python-setuptools wamerican
+# install some basic packages we need
+sudo apt-get -y install ant ant-optional git libev-dev libyaml-dev python-dev python-setuptools rsync screen wamerican
 # install some python modules that we need
 sudo pip install blist cassandra-driver cql decorator futures nose-test-select pycassa
 # ..use the latest ccm HEAD
