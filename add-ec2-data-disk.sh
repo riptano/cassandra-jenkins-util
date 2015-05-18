8514e8c465db18a800323ff1356a7228bdf66e41
Ariel Weisberg <ariel@weisberg.ws>
2015-05-18 17:51:19 -0400

    Mount with noatime,nodiratime, make it possible to mount with or without write barriers

--- add-ec2-data-disk.sh
+++ add-ec2-data-disk.sh
@@ -1,10 +1,15 @@
 #!/bin/sh -xe
 PARTITION="xvdb"
 MOUNTPOINT="/mnt"
+MOUNT_OPTIONS=" -o noatime,nodiratime"
+if [ -n "$BARRIER" ]; then
+    MOUNT_OPTIONS="$MOUNT_OPTIONS,barrier=$BARRIER ";
+fi
+if [ -z "$BARRIER" ]
 if grep $PARTITION /proc/partitions; then
     sudo mkfs.ext4 -F /dev/$PARTITION
     [ -d $MOUNTPOINT ] || sudo mkdir $MOUNTPOINT
-    sudo mount /dev/$PARTITION $MOUNTPOINT
+    sudo mount $MOUNT_OPTIONS /dev/$PARTITION $MOUNTPOINT
     sudo mkdir -m 1777 $MOUNTPOINT/tmp
     sudo sh -c "echo \"export TMPDIR=$MOUNTPOINT/tmp\" >> /etc/profile"
     echo "Done Setting Up Data Disk."
--- add-gce-data-disk-v2.sh
+++ add-gce-data-disk-v2.sh
@@ -1,11 +1,15 @@
 #!/bin/sh -xe
 PARTITION="sda2"
 MOUNTPOINT="/mnt"
+MOUNT_OPTIONS=" -o noatime,nodiratime"
+if [ -n "$BARRIER" ]; then
+    MOUNT_OPTIONS="$MOUNT_OPTIONS,barrier=$BARRIER ";
+fi
 if grep $PARTITION /proc/partitions; then
     sudo umount $MOUNTPOINT || /bin/true
     sudo mkfs.ext4 /dev/$PARTITION
     [ -d $MOUNTPOINT ] || sudo mkdir $MOUNTPOINT
-    sudo mount /dev/$PARTITION $MOUNTPOINT
+    sudo mount $MOUNT_OPTIONS /dev/$PARTITION $MOUNTPOINT
     sudo mkdir $MOUNTPOINT/tmp
     sudo chmod 777 $MOUNTPOINT/tmp
     echo "Done Setting Up Data Disk."
