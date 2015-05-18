e110934ee389b2c75d7e83de09624de9dcbe0198
Michael Shuler <michael@pbandjelly.org>
2015-05-18 17:58:50 -0500

    Remount / with noatime,nodiratime,barrier=0

--- add-ec2-data-disk.sh
+++ add-ec2-data-disk.sh
@@ -7,7 +7,10 @@ if grep $PARTITION /proc/partitions; then
     sudo mount /dev/$PARTITION -o noatime,nodiratime,barrier=0 $MOUNTPOINT
     sudo mkdir -m 1777 $MOUNTPOINT/tmp
     sudo sh -c "echo \"export TMPDIR=$MOUNTPOINT/tmp\" >> /etc/profile"
+    . /etc/profile
     echo "Done Setting Up Data Disk."
+    sudo mount /dev/xvda1 -o remount,noatime,nodiratime,barrier=0 /
+    echo "Remounted / with noatime,nodiratime,barrier=0"
 else
     echo "Data Partition Not Found!"
     exit 1
