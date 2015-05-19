64977518c9d84125e450e5a58687410e1b40879d
Michael Shuler <michael@pbandjelly.org>
2015-05-19 11:48:17 -0500

    Reorder mount options

--- add-ec2-data-disk.sh
+++ add-ec2-data-disk.sh
@@ -4,12 +4,12 @@ MOUNTPOINT="/mnt"
 if grep $PARTITION /proc/partitions; then
     sudo mkfs.ext4 -F /dev/$PARTITION
     [ -d $MOUNTPOINT ] || sudo mkdir $MOUNTPOINT
-    sudo mount /dev/$PARTITION -o noatime,nodiratime,barrier=0 $MOUNTPOINT
+    sudo mount -o noatime,nodiratime,barrier=0 /dev/$PARTITION $MOUNTPOINT
     sudo mkdir -m 1777 $MOUNTPOINT/tmp
     sudo sh -c "echo \"export TMPDIR=$MOUNTPOINT/tmp\" >> /etc/profile"
     . /etc/profile
     echo "Done Setting Up Data Disk."
-    sudo mount /dev/xvda1 -o remount,noatime,nodiratime,barrier=0 /
+    sudo mount -o remount,noatime,nodiratime,barrier=0 /dev/xvda1 /
     echo "Remounted / with noatime,nodiratime,barrier=0"
 else
     echo "Data Partition Not Found!"
