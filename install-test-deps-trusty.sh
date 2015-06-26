d76e120a92d9e43df8efb8a2af2776fe706f8292
Michael Shuler <michael@pbandjelly.org>
2015-06-26 15:01:01 -0500

    Drop java -version - it may not be installed

--- install-test-deps-trusty.sh
+++ install-test-deps-trusty.sh
@@ -2,9 +2,6 @@
 # fix default umask of 0002 for hadoop data dir errors
 sudo sh -c 'echo "umask 0022" >> /etc/profile'
 
-# which jdk do we have?
-java -version
-
 # install some basic packages we need
 sudo apt-get -y install ant ant-optional git libev-dev libyaml-dev lsof python-dev python-setuptools python-pip rsync screen wamerican
 # install some python modules that we need
