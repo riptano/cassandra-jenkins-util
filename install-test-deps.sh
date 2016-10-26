790cd8ae02815bbd98e8244d8f28e4c599919ec9
Michael Shuler <michael@pbandjelly.org>
2016-10-25 23:04:27 -0500

    Export CASS_DRIVER env vars for all users in /etc/profile

--- install-test-deps.sh
+++ install-test-deps.sh
@@ -1,7 +1,12 @@
 #!/bin/bash -xe
 
 # fix default umask of 0002 for hadoop data dir errors
-sudo sh -c 'echo "umask 0022" >> /etc/profile'
+# export python-driver cython suppression env vars to all users (`sudo pip install ..` needs root to have this env, too)
+sudo sh -c 'cat << EOF >> /etc/profile
+umask 0022
+export CASS_DRIVER_NO_CYTHON="true"
+export CASS_DRIVER_NO_EXTENSIONS="true"
+EOF'
 
 # install some basic packages we need
 sudo apt-get -y install ant ant-optional git libev-dev libyaml-dev lsof python-dev python-setuptools python-pip rsync screen wamerican
